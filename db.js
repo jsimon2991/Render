7176c9e0aed287c8c51e9ef212ccfbb1beb2f29b
jsimon2991 <164003985+jsimon2991@users.noreply.github.com>
2024-09-25 12:04:44 +0200

    fixe db variable

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  proces.env.DATABASE_NAME, // Database name
-  proces.env.DATABASE_USERNAME, // User
-  proces.env.DATABASE_PASSWORD, // Password
+  process.env.DATABASE_NAME, // Database name
+  process.env.DATABASE_USERNAME, // User
+  process.env.DATABASE_PASSWORD, // Password
   {
-    host: proces.env.DATABASE_HOST, // Host
+    host: process.env.DATABASE_HOST, // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
